72e1c37ae256361392090045971026499fb370ea
kozlov-ae <allek-007@list.ru>
2020-10-23 17:22:30 +0300

    На работе из VisualCode

--- AspEducation/Models/ViewComponents/SidebarViewComponent.cs
+++ AspEducation/Models/ViewComponents/SidebarViewComponent.cs
@@ -1,7 +1,21 @@
+using System.Threading.Tasks;
+using AspEducation.Domain;
+using Microsoft.AspNetCore.Mvc;
+
 namespace AspEducation.Models.ViewComponents
 {
-    public class SidebarViewComponent
+    public class SidebarViewComponent : ViewComponent
     {
-        
+        private readonly DataManager datamanager;
+
+        SidebarViewComponent(DataManager datamanager)
+        {
+            this.datamanager = datamanager;
+        }
+
+        public Task<ViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult) View("Default", datamanager.ServiceItems.GetServiceItems()));
+        }
     }
 }
